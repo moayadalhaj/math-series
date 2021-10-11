dc5c2cdcd0b09ab313c958468b5186ecaee7ab9b
moayadalhaj <moayad.alhaj21@gmail.com>
2021-10-11 18:51:02 +0300

    test: added tests to test the lucas function

--- tests/test_series.py
+++ tests/test_series.py
@@ -29,3 +29,33 @@ def test_fibonacci3():
     result=series.fibonacci(number)
     #Aseert
     assert result==accepted_value
+
+def test_lucas():
+    #Arrange
+    number=5
+    accepted_value=11
+    #Act
+    result=series.lucas(number)
+    #Aseert
+    assert result==accepted_value
+
+def test_lucas2():
+    #Arrange
+    number=1
+    accepted_value=1
+    #Act
+    result=series.lucas(number)
+    #Aseert
+    assert result==accepted_value
+
+def test_lucas3():
+    """
+    To try if the result not equal accepted value
+    """
+    #Arrange
+    number=7
+    accepted_value=12
+    #Act
+    result=series.lucas(number)
+    #Aseert
+    assert result==accepted_value
