e2906077329ed957f75356d06609ad22aa449c46
moayadalhaj <moayad.alhaj21@gmail.com>
2021-10-11 18:44:43 +0300

    test: added tests to test the fibonacci function

---
+++ tests/test_series.py
@@ -0,0 +1,28 @@
+from math_series import series
+
+def test_fibonacci():
+    #Arrange
+    number=7
+    accepted_value=13
+    #Act
+    result=series.fibonacci(number)
+    #Aseert
+    assert result==accepted_value
+
+def test_fibonacci2():
+    #Arrange
+    number=1
+    accepted_value=1
+    #Act
+    result=series.fibonacci(number)
+    #Aseert
+    assert result==accepted_value
+
+def test_fibonacci3():
+    #Arrange
+    number=7
+    accepted_value=12
+    #Act
+    result=series.fibonacci(number)
+    #Aseert
+    assert result==accepted_value
