22af9bd5d98c25ec2bc06cc4a321fc02ea4506c0
moayadalhaj <moayad.alhaj21@gmail.com>
2021-10-11 18:55:14 +0300

    feat: add sum_series function that tak one required arguments and two opitional that produce any series according to the opitional arguments

--- math_series/series.py
+++ math_series/series.py
@@ -17,3 +17,11 @@ def lucas(n):
         return n
     else:
         return lucas(n-2)+lucas(n-1)
+
+def sum_series(n,a=0,b=1):
+    if n==0:
+        return a
+    if n==1:
+        return b
+    else:
+        return sum_series(n-2,a,b)+sum_series(n-1,a,b)
