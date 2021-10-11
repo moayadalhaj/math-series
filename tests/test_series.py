e79cf1c7d8951684f2170138c5b8adcb09f59a75
moayadalhaj <moayad.alhaj21@gmail.com>
2021-10-11 19:02:52 +0300

    test: added tests to test the sum_series for different optional and nth values

--- tests/test_series.py
+++ tests/test_series.py
@@ -59,3 +59,33 @@ def test_lucas3():
     result=series.lucas(number)
     #Aseert
     assert result==accepted_value
+
+def test_sum_series():
+    #Arrange
+    number=5
+    accepted_value=11
+    #Act
+    result=series.sum_series(number,2,1)
+    #Aseert
+    assert result==accepted_value
+
+def test_sum_series2():
+    #Arrange
+    number=7
+    accepted_value=13
+    #Act
+    result=series.sum_series(number)
+    #Aseert
+    assert result==accepted_value
+
+def test_sum_series3():
+    """
+    To try if the result not equal accepted value
+    """
+    #Arrange
+    number=0
+    accepted_value=1
+    #Act
+    result=series.sum_series(number,2,0)
+    #Aseert
+    assert result==accepted_value
